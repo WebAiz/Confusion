9710a14805e0a553f2e4046418928fee52e14cc8
Aizada <webaiz>
2021-01-19 14:02:29 +0600

    no json-server initial setup

--- src/shared/baseUrl.js
+++
@@ -1,8 +0,0 @@
-export const baseUrl = 'http://localhost:3001/';
-
-// export const baseUrl1 = 'https://jsonkeeper.com/b/XRQP/'
-// export const baseUrlComments = "https://jsonkeeper.com/b/UFNQ/"
-// export const baseUrlDishes = "https://jsonkeeper.com/b/9PIT/"
-// export const baseUrlLeaders = "https://jsonkeeper.com/b/BI2U/"
-// export const baseUrlPromotions = "https://jsonkeeper.com/b/111Z/"
-// export const baseUrlFeedbacks = "https://jsonkeeper.com/b/VM81/"
